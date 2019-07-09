76a94b6cea5ab4e0804117e16799501ad53351b9
Lando <lando.schumpich@gmail.com>
2019-07-09 14:50:39 +0200

    Projektdateien hinzufügen.

---
+++ Octopus/OctopusCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace Octopus
+{
+    public class OctopusCommand : Command
+    {
+        public OctopusCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static OctopusCommand Instance
+        {
+            get; private set;
+        }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName
+        {
+            get { return "OctopusCommand"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            RhinoApp.WriteLine("The {0} command is under construction.", EnglishName);
+
+            return Result.Success;
+        }
+    }
+}
